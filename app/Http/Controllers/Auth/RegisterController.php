5c4cca459129d4d0d6ac26fd1d70f8b0173a4e5f
Toby Evans <tobz.nz@gmail.com>
2019-08-07 22:10:37 +1200

    Fix up register form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
@@ -82,7 +84,7 @@ class RegisterController extends Controller
     {
         $validator = $this->validator($request->all())->validate();
 
-        event(new Registered($user = $this->create($validator->validated())));
+        event(new Registered($user = $this->create($validator)));
 
         $this->guard()->login($user);
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,13 +3,17 @@
 @section('content')
 
 <div class="flex flex-col justify-center items-center">
-    <img class="block mt-8" width="145" height="145" src="{{ asset('images/icon.svg') }}">
-    <img class="block mb-5" width="320" height="100" src="{{ asset('images/logo.svg') }}">
+    <svg class="block mt-6" style="color:var(--blue-1)" width="145" height="145">
+        <use href="{{ asset('images/icons.svg#icon') }}"></use>
+    </svg>
+    <svg class="mb-5" width="300" height="80">
+        <use href="{{ asset('images/icons.svg#logo') }}"></use>
+    </svg>
 
-    <form method="POST" action="{{ route('register') }}">
+    <form class="mb-6" method="POST" action="{{ route('register') }}">
         @csrf
 
-        <div class="input-group mb-4">
+        <div class="input-group grid mb-4">
             <label class="input-label" for="name">{{ __('Name') }}</label>
             <input class="input-field" type="text" id="name" name="name" value="{{ old('name') }}" placeholder="e.g. Arthur Dent" required autofocus autocomplete="name">
             @if ($errors->has('name'))
@@ -17,7 +21,7 @@
             @endif
         </div>
 
-        <div class="input-group mb-4">
+        <div class="input-group grid mb-4">
             <label class="input-label" for="email">{{ __('Email Address') }}</label>
             <input class="input-field" type="email" id="email" name="email" value="{{ old('email') }}" placeholder="e.g. user@tankful.nz" required autocomplete="username">
             @if ($errors->has('email'))
@@ -25,7 +29,7 @@
             @endif
         </div>
 
-        <div class="input-group mb-4">
+        <div class="input-group grid mb-4">
             <label class="input-label" for="password">{{ __('Password') }}</label>
             <input class="input-field" type="password" id="password" name="password" placeholder="e.g. I have a long strong password" required>
             @if ($errors->has('password'))
@@ -33,24 +37,38 @@
             @endif
         </div>
 
-        <div class="input-group mb-4">
+        <div class="input-group grid mb-4">
             <label class="input-label" for="password_confirmation">{{ __('Confirm Password') }}</label>
             <input class="input-field" type="password" id="password_confirmation" name="password_confirmation" required>
         </div>
 
-        <div class="input-group mb-4">
-            <label class="input-label" for="address">{{ __('Address') }}</label>
-            <input class="input-field" type="text" id="address" name="address" data-places required>
+        <div class="input-group grid mb-4 @if($errors->has('dimensions.address'))input--invalid @endif">
+            <label class="input-label" for="place">{{ __('Address') }}</label>
+            <places-field class="input-field" id="place" name="place" data-field="address-location" value="{{ old('place') }}" placeholder="e.g. 12 Kagan Ave, Mangawhai" data-place required></places-field>
+
+            <input type="hidden" id="address-name" name="address[name]" value="{{ old('address.name') }}">
+            <input type="hidden" id="address-city" name="address[city]" value="{{ old('address.city') }}">
+            <input type="hidden" id="address-country" name="address[country]" value="{{ old('address.country') }}">
+            <input type="hidden" id="address-postcode" name="address[postcode]" value="{{ old('address.postcode') }}">
+            <input type="hidden" id="address-latlng-lat" name="address[latlng][lat]" value="{{ old('address.latlng.lat') }}">
+            <input type="hidden" id="address-latlng-lng" name="address[latlng][lng]" value="{{ old('address.latlng.lng') }}">
+
+            @if ($errors->has('address') || $errors->has('place'))
+                <div class="input--error">{{ $errors->first('place') ?: $errors->first('address') }}</div>
+            @endif
         </div>
 
-        <div class="input-group mb-4">
+        <div class="input-group grid mb-6">
             <label class="input-label" for="household-size">{{ __('Household Size(number of residents)') }}</label>
-            <input class="input-field" type="number" min="1" id="household-size" name="household_size" required>
+            <input class="input-field" type="number" min="1" id="household-size" name="household_size" value="{{ old('household_size', 3) }}" required>
         </div>
 
         <button type="submit" class="w-full button button--primary border">{{ __('Register') }}</button>
 
+        <a href="{{ route('login') }}" class="flex p-4 link justify-center">or Login in</a>
     </form>
 </div>
 
 @endsection
+
+@push('scripts', '<script src="https://cdn.jsdelivr.net/npm/places.js@1.16.4"></script>')
