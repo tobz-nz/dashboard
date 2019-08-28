e40a3049da3ab348b84f6122c49e125bf050000e
Toby Evans <tobz.nz@gmail.com>
2019-08-28 23:17:17 +1200

    Playing around with subgrid
    So bloody good. I can easily layout my form how I want to now… well, soon… ish.
    Only Firefox developer beta’s have support right now.

--- resources/views/devices/edit.blade.php
+++ resources/views/devices/edit.blade.php
@@ -12,12 +12,12 @@
     </header>
 
     <div class="pt-7 px-8 flex flex-col items-start">
-        <form class="w-full" action="{{ route('devices.update', $device) }}" method="post">
+        <form class="device-form w-full" action="{{ route('devices.update', $device) }}" method="post">
             @method('patch')
 
             @include('devices.form')
 
-            <div class="flex justify-between">
+            <div class="flex justify-between" style="grid-column:1/-1">
                 <button type="submit" class="mb-7 button">Update Device</button>
                 <button form="delete-form" type="submit" class="mb-7 button--link danger" onclick="return confirm('Are you REALLY Sure?')">Delete Device</button>
             </div>
