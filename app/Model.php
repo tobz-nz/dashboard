b2e900eb3ff6dc4b4e6f41fb138f105c960789d3
Toby Evans <tobz.nz@gmail.com>
2019-08-31 22:41:40 +1200

    Should fix date format issues

--- app/Model.php
+++ app/Model.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Database\PostgresConnection;
 
 class Model extends BaseModel
 {
@@ -10,4 +11,16 @@ class Model extends BaseModel
      * @inheritdoc
      */
     protected $dateFormat = 'Y-m-d H:i:sO';
+
+    /**
+     * @inheritdoc
+     */
+    public function getDateFormat()
+    {
+        if ($this->getConnection() instanceof PostgresConnection) {
+            return $this->dateFormat ?: parent::getDateFormat();
+        }
+
+        parent::getDateFormat();
+    }
 }
