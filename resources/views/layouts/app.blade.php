ce79a5ef1788fa44e5171fb17ba494b55f01fb3e
Toby Evans <tobz.nz@gmail.com>
2019-09-14 00:39:46 +1200

    Fix ziggy directive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     @stack('styles')
 
     <!-- Scripts -->
-    @routes
+    @routes()
     <script src="{{ asset('js/manifest.js') }}"></script>
     <script src="{{ asset('js/vendor.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
