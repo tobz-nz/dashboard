956e328cb0622f1453077317c9791c95726c352b
Toby Evans <tobz.nz@gmail.com>
2019-09-22 16:36:08 +1200

    Remove double up view

--- resources/views/components/input-field.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="mb-4 input-group inline items-center @if($errors->has($name))input--invalid @endif">
-    <label class="input-label" for="{{ $id ?? $name }}">{{ __('Name') }}</label>
-    <input class="input-field" type="text" id="{{ $id ?? $name }}" name="{{ $name }}" value="{{ old($name, $value ?? null) }}" {!! \Illuminate\Support\Arr::html_attributes($attributes??[]) !!}>
-    <div class="input-summary">{{ $slot }}</div>
-    @if ($errors->has($name))
-        <div class="input--error">{{ $errors->first($name) }}</div>
-    @endif
-</div>
