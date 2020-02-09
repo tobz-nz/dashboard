be135ef81aee7eeac5343e000a3b0f109a34f895
Toby Evans <tobz.nz@gmail.com>
2020-02-09 21:00:43 +1300

    css tweaks

--- resources/views/components/device-selector.blade.php
+++ resources/views/components/device-selector.blade.php
@@ -10,7 +10,10 @@
     </summary>
     <div>
         @foreach($devices as $device)
-        <a href="{{ route('devices.show', $device) }}" class="link" title="{{ $device->name }}">{{ $device->name }}</a>
+        <a href="{{ route('devices.show', $device) }}" class="link" title="{{ $device->name }}">
+            <span class="block w h" style="--size:20px;--bgc:{{ $device->color }}"></span>
+            {{ $device->name }}
+        </a>
         @endforeach
         @can('create', App\Device::class)
         <a href="{{ route('devices.create') }}" class="pt-3 border-t link link--new" title="Add new Water Monitor">Add new Water Monitor</a>
